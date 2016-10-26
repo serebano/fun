39e85d9e7a7b249e257ca4490135db57378667c8
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2016-10-26 04:59:51 +0300

    dist

--- examples/index.js
+++ examples/index.js
@@ -1,16 +1,58 @@
-const create = require('../')
-const logger = require('./logger')
 
-// the function
-const inc = n => n + 1
+const loadingAction = (x) => ({ state }) => state.set('loading', x)
 
-// middlewares
-const multi = next => n => next(n * 3)
-const toNumber = next => n => next(Number(n))
+const fetchData = ({ input, state, path }) => {
+    fetch('http://asdasdasd')
+        .then(items => path.success({ items }))
+        .catch(err => path.error({ err: err.message }))
+}
 
-const foo = create(inc, multi, toNumber, logger)
+const setError = ({ input, state }) => state.set('error', input.err)
+const setItems = ({ input, state }) => state.set('app.items', input.items)
 
-const bar = create(inc)
-bar.use(logger)
+const loginClicked = [
+    loadingAction(true),
+    [
+        fetchData, {
+            success: [ setItems ],
+            error: [ setError ]
+        }
+    ],
+    loadingAction(false)
+]
 
-console.log(foo('10'), bar(foo('2')))
+loginClicked({ username: 'andrei', pass: 'asdasd' })
+
+<app(props, box) export default>
+    <section switch(page)>
+        <div case='home' text>Hello ${xxx} world</div>
+        <div case='contact'>
+            <h1>"Contct"</h1>
+            <list-item id=1 filter='foo' />
+        </div>
+        <p default>foo foo<p>
+    </section>
+    <button on-click=loginClicked>'Login'</button
+    <button on-click=[render, { page: 'contact' }] text>contact</button>
+</app>
+
+<render(props = { page: 'home' })>
+    <sss root=document.body page=state.page />
+</render>
+
+app = firebase.database('asdsadsd/asdasd')
+
+export const state = {
+    items: 'app.items'
+}
+
+app.set('app.items', [1,2,3])
+
+app.onValue('app.items', snap => {
+    render({
+        page: 'videos',
+        items: snap.val()
+    })
+})
+
+app.onChildAdded('app.items', )
